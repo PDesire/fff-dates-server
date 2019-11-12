3d7509beebb18566392a91ee43d43b18a30f5c0e
Tristan Marsell <tristan@sqrls.de>
2019-11-12 18:21:56 +0100

    Normalize URLs & remove their crap

--- app.js
+++ app.js
@@ -15,11 +15,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/fff/get', async function (req, res, next) {
     var parsed = await csv().fromStream(request.get('http://fridaysforfuture.de/map/mapdata-29.csv'));
+
+    for (var a = 0; a < parsed.length; a++) {
+        if (parsed[a]['Instagram'] != "") parsed[a]['Instagram'] = normalizeURLs(parsed[a]['Instagram']);
+        if (parsed[a]['Facebook event'] != "") parsed[a]['Facebook event'] = normalizeURLs(parsed[a]['Facebook event']);
+        if (parsed[a]['Twitter'] != "") parsed[a]['Twitter'] = normalizeURLs(parsed[a]['Twitter']);
+        if (parsed[a]['Facebook'] != "") parsed[a]['Facebook'] = normalizeURLs(parsed[a]['Facebook']);
+    }
+
     res.status(200).send({
         success: 'true',
         message: parsed
     });
 });
 
+function normalizeURLs(url) {
+    return url.split('?')[0];
+}
 
 module.exports = app;
