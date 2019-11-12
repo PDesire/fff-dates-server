249b187784a3f12428897f00b061f97d9733ca65
Tristan Marsell <tristan@sqrls.de>
2019-11-12 18:43:00 +0100

    Add a parameter to search for specific values

--- app.js
+++ app.js
@@ -29,6 +29,30 @@ app.get('/fff/get', async function (req, res, next) {
     });
 });
 
+app.get('/fff/search/:parameter/:value', async function (req, res, next) {
+    var parameter = req.params.parameter;
+    var value = req.params.value;
+
+    var filtered = [];
+    var parsed = await csv().fromStream(request.get('http://fridaysforfuture.de/map/mapdata-29.csv'));
+
+    for (var a = 0; a < parsed.length; a++) {
+        if (parsed[a]['Instagram'] != "") parsed[a]['Instagram'] = normalizeURLs(parsed[a]['Instagram']);
+        if (parsed[a]['Facebook event'] != "") parsed[a]['Facebook event'] = normalizeURLs(parsed[a]['Facebook event']);
+        if (parsed[a]['Twitter'] != "") parsed[a]['Twitter'] = normalizeURLs(parsed[a]['Twitter']);
+        if (parsed[a]['Facebook'] != "") parsed[a]['Facebook'] = normalizeURLs(parsed[a]['Facebook']);
+
+        if (parsed[a][parameter].includes(value)) {
+            filtered.push(parsed[a]);
+        }
+    }
+
+    res.status(200).send({
+        success: 'true',
+        message: filtered
+    });
+});
+
 function normalizeURLs(url) {
     return url.split('?')[0];
 }
