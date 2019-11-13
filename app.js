e9f75a6a765b3adf1f68f563dbefc26d72373022
Tristan Marsell <tristan@sqrls.de>
2019-11-13 16:53:55 +0100

    Let's play Datenmasseuse

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/fff/get', async function (req, res, next) {
+app.get('/fff/v1/get', async function (req, res, next) {
     var parsed = await csv().fromStream(request.get('http://fridaysforfuture.de/map/mapdata-29.csv'));
 
     for (var a = 0; a < parsed.length; a++) {
@@ -29,7 +29,23 @@ app.get('/fff/get', async function (req, res, next) {
     });
 });
 
-app.get('/fff/search/:parameter/:value', async function (req, res, next) {
+app.get('/fff/v2/get', async function (req, res, next) {
+    var parsed = await csv().fromStream(request.get('http://fridaysforfuture.de/map/mapdata-29.csv'));
+
+    for (var a = 0; a < parsed.length; a++) {
+        if (parsed[a]['Instagram'] != "") parsed[a]['Instagram'] = normalizeURLs(parsed[a]['Instagram']);
+        if (parsed[a]['Facebook event'] != "") parsed[a]['Facebook event'] = normalizeURLs(parsed[a]['Facebook event']);
+        if (parsed[a]['Twitter'] != "") parsed[a]['Twitter'] = normalizeURLs(parsed[a]['Twitter']);
+        if (parsed[a]['Facebook'] != "") parsed[a]['Facebook'] = normalizeURLs(parsed[a]['Facebook']);
+    }
+
+    res.status(200).send({
+        success: 'true',
+        message: createNormalizedObject(parsed)
+    });
+});
+
+app.get('/fff/v1/search/:parameter/:value', async function (req, res, next) {
     var parameter = req.params.parameter;
     var value = req.params.value;
 
@@ -53,8 +69,55 @@ app.get('/fff/search/:parameter/:value', async function (req, res, next) {
     });
 });
 
+app.get('/fff/v2/search/:parameter/:value', async function (req, res, next) {
+    var parameter = req.params.parameter;
+    var value = req.params.value;
+
+    var filtered = [];
+    var parsed = await csv().fromStream(request.get('http://fridaysforfuture.de/map/mapdata-29.csv'));
+
+    for (var a = 0; a < parsed.length; a++) {
+        if (parsed[a]['Instagram'] != "") parsed[a]['Instagram'] = normalizeURLs(parsed[a]['Instagram']);
+        if (parsed[a]['Facebook event'] != "") parsed[a]['Facebook event'] = normalizeURLs(parsed[a]['Facebook event']);
+        if (parsed[a]['Twitter'] != "") parsed[a]['Twitter'] = normalizeURLs(parsed[a]['Twitter']);
+        if (parsed[a]['Facebook'] != "") parsed[a]['Facebook'] = normalizeURLs(parsed[a]['Facebook']);
+
+        if (parsed[a][parameter].includes(value)) {
+            filtered.push(parsed[a]);
+        }
+    }
+
+    res.status(200).send({
+        success: 'true',
+        message: createNormalizedObject(filtered)
+    });
+});
+
 function normalizeURLs(url) {
     return url.split('?')[0];
+}
+
+function createNormalizedObject(rawData) {
+    var normalizedArray = [];
+    for (var a = 0; a < rawData.length; a++) {
+        normalizedArray.push({
+            lat: rawData[a]['lang'],
+            lon: rawData[a]['lat'],
+            full_info: rawData[a]['Name'] + " " + rawData[a]['Startpunkt'] + " - " + rawData[a]['Uhrzeit'],
+            time: rawData[a]['Uhrzeit'],
+            city: rawData[a]['Name'],
+            state: rawData[a]['Bundesland'],
+            start: rawData[a]['Startpunkt'],
+            additionals: rawData[a]['zusatzinfo'],
+            facebook: rawData[a]['Facebook'],
+            facebook_event: rawData[a]['Facebook event'],
+            twitter: rawData[a]['Twitter'],
+            instagram: rawData[a]['Instagram'],
+            website: rawData[a]['website']
+        })
+    }
+
+    return normalizedArray;
 }
 
 module.exports = app;
